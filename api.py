afaafa58b230e51690fa959a4d298ebbb172c482
Gabriel Avelar <hotmartgabrielalencar@outlook.com>
2025-07-15 16:48:17 -0300

    Corrige porta para rodar no Heroku

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from bd import adicionar_clientes, listar_clientes
+import os
 
 app = Flask(__name__)
 
@@ -29,5 +30,7 @@ def obter_clientes():
         return jsonify({"erro": str(e)}), 500
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # pega a porta do Heroku ou usa 5000 localmente
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
