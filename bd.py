30160dbdde1b98566ec714f9fd92b5c1aa963c36
Gabriel Avelar <hotmartgabrielalencar@outlook.com>
2025-07-18 23:06:26 -0300

    ajuste para rede privada Railway2

--- bd.py
+++ bd.py
@@ -1,16 +1,30 @@
 import os
 import mysql.connector
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 load_dotenv()
 
-db_config = {
-    'host': os.getenv("DB_HOST", "mysql.ferrovia.interno"),
-    'user': os.getenv("DB_USER"),
-    'password': os.getenv("DB_PASSWORD"),
-    'database': os.getenv("DB_NAME"),
-    'port': int(os.getenv("DB_PORT", 3306))
-}
+# Usa DATABASE_URL se existir, senão cai no modo tradicional
+database_url = os.getenv("DATABASE_URL")
+
+if database_url:
+    url = urlparse(database_url)
+    db_config = {
+        'host': url.hostname,
+        'user': url.username,
+        'password': url.password,
+        'database': url.path[1:],  # remove a "/" do início
+        'port': url.port or 3306
+    }
+else:
+    db_config = {
+        'host': os.getenv("DB_HOST", "localhost"),
+        'user': os.getenv("DB_USER"),
+        'password': os.getenv("DB_PASSWORD"),
+        'database': os.getenv("DB_NAME"),
+        'port': int(os.getenv("DB_PORT", 3306))
+    }
 
 
 def listar_clientes():
@@ -30,7 +44,6 @@ def adicionar_clientes(cliente):
         INSERT INTO clientes (nome, endereco, bairro, telefone, suco_preferido, obs)
         VALUES (%s, %s, %s, %s, %s, %s)
     """
-
     valores = (
         cliente.get("nome"),
         cliente.get("endereco"),
@@ -39,7 +52,6 @@ def adicionar_clientes(cliente):
         cliente.get("suco_preferido"),
         cliente.get("obs", "")
     )
-
     cursor.execute(sql, valores)
     conexao.commit()
     cursor.close()
